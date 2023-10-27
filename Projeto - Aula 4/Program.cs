8b5c9fa1da5c81ab3e760abb5bd9a130d277b4ff
Marcus Vinícius Pires <136832870+marvipi@users.noreply.github.com>
2023-10-26 21:23:09 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto - Aula 4/Program.cs
@@ -0,0 +1,35 @@
+﻿
+var nome = LerNome();
+
+var dataDeNascimento = PerguntarDataDeNascimento();
+
+var aniversarioCentenario = AniversarioCentenario(dataDeNascimento);
+
+Console.WriteLine($"Você viverá até {aniversarioCentenario.ToShortDateString()}");
+
+string LerNome()
+{
+	Console.Write("Digite o seu nome: ");
+	var nome = Console.ReadLine();
+	return nome == string.Empty || nome == null
+		? "Anônimo"
+		: nome;
+}
+
+DateTime PerguntarDataDeNascimento()
+{
+	Console.Write("Digite a sua data de nascimento: ");
+	var entrada = Console.ReadLine();
+	while (!DateTime.TryParse(entrada, out _))
+	{
+		Console.Write("Digite a sua data de nascimento: ");
+		entrada = Console.ReadLine();
+	}
+	return DateTime.Parse(entrada);
+}
+
+DateTime AniversarioCentenario(DateTime dataDeNascimento)
+{
+	var aniversarioCentenario = dataDeNascimento.AddYears(100);
+	return aniversarioCentenario;
+}
