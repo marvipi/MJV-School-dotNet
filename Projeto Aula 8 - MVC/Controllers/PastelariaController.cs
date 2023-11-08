8f04988d8f168a38284c3ae29334e71b0129b8d9
Marcus Vinícius Pires <136832870+marvipi@users.noreply.github.com>
2023-11-07 21:11:09 -0300

    Implemente o PastelariaController

---
+++ Projeto Aula 8 - MVC/Controllers/PastelariaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_Aula_8___MVC.Controllers;
+public class PastelariaController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult Menu()
+	{
+		return View();
+	}
+}
